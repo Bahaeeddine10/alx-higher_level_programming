dc4de7710c93b705e67bafa833331def1464d762
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-10-31 19:26:23 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for a in range(10):
-    for b in range(a, 10):
-        if a < b:
-    print("{:d}{:d}".format(a, b), end="\n"
-        if a == 8 and b == 9 else ", ")
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i * 10 + j < 89:
+            print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}".format(89))
