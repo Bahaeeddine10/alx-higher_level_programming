9a73da2f730e2fa690697fa41dab9d2dc082e844
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-10-31 19:22:00 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for tebahpla in range(25, -1, -1):
-    c = tebahpla + ord('A')
-    if tebahpla % 2 == 1:
-        c+ = 32
-    print("{:c}".format(c), end="")
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
+
