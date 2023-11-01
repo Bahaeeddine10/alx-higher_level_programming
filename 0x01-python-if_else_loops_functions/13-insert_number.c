1ff5a46bf84af9de3a164d4f070551c18bbbb6da
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-10-31 19:29:30 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
  * insert_node - Inserts a number into a sorted singly-linked list.
@@ -11,30 +10,25 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *node = *head, *new = malloc(sizeof(listint_t));
+        listint_t *node = *head, *new;
 
-        if (!new)
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
                 return (NULL);
-
         new->n = number;
-        new->next = NULL
 
-        if (!node  || new->n < node->n)
+        if (node == NULL || node->n >= number)
         {
                 new->next = node;
-                return (*head = new);
+                *head = new;
+                return (new);
         }
 
-        while (node)
+        while (node && node->next && node->next->n < number)
+                node = node->next;
 
-        if (node->next || new->n < node->next->n)
-            {
-                new->next = node->next;
-                node->next = new;
-                return(node);
-            }
-
-        node = node->next;
+        new->next = node->next;
+        node->next = new;
 
         return (new);
 }
