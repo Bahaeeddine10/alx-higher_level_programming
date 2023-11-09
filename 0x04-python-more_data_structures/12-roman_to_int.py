0830ac4313869b37498891534c060157ad8a44e7
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-11-08 17:50:18 -0800

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-  total = 0
-  num = 0
-  roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-  for r in reversed(roman_string):
+    total = 0
+    num = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
       num = roman[r]
       total += num if total < num * 5 else -num
-  return total
+    return total
