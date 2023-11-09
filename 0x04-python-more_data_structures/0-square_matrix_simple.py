116e27ff7a7d33a00bd18d96c7dbfaf3c4555167
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-11-08 17:17:37 -0800

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda new_matrix:list(map(lambda m: m**2, new_matrix)), matrix))
+    return list(map(lambda newMat:list(map(lambda m: m**2, newMat)), matrix))
