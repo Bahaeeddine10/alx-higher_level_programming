245872f22172a89f6812553f21e2940281f1a483
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-11-21 17:52:19 -0800

    0x06. Python - Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,15 +7,17 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
+
         Raises:
             TypeError: if size is not an integer
             ValueError: if size is not >= 0
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
