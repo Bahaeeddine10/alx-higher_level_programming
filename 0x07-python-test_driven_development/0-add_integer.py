ae80e5f3d131ee09571847d36dc05dde00567a8a
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-11-27 15:45:20 -0800

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+  """Defines an integer addition function."""
+  
+def add_integer(a, b=98):
+    """Adds two integers.
+
+  Args:
+      a: first integer.
+      b: second integer, default 98.
+
+  Raises:
+      TypeError: If either of a or b is a non-integer and non-float.
+
+  Returns:
+      The sum of the two integers.
+    """
+  
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
