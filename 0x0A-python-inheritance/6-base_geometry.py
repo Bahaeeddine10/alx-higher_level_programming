509be7c0eeeea42e7118c9d747d071d14615c724
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-12-06 15:39:45 -0800

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''Module for BaseGeometry class.'''
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
     """Represent base geometry."""
+
     def area(self):
         """Not implemented."""
         raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''Module for BaseGeometry class.'''
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
     """Reprsent base geometry."""
+
     def area(self):
         """Not yet implemented."""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
+
         Args:
             name (str): The name of the parameter.
             value (int): The parameter to validate.
