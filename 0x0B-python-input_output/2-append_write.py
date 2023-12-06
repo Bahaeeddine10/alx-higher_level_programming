0efcb1d76bacb55ddd197f380c9f76d3b1d33700
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-12-06 15:15:09 -0800

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file."""
+    """Appends filename with UTF8"""
      with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
