0d1308d15381716fdb89860afdf60e8a16319984
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-12-11 15:22:58 -0800

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON serialization of a list of dictionaries."""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return dumps(list_dictionaries)
